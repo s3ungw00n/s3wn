e9cda5f367080c1a1ff8f4935c8bb06f2f1a2047
LeeSeungWoon <s3wn.code@gmail.com>
2022-06-11 23:37:46 +0900

    Greeting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Styles } from 'sveltestrap';
+</script>
+
+<Styles />
+<header class="header">
+	<div class="greeting px-4">
+		<div>{"Hello, There. I'm"}</div>
+		<h1>SeungWoon Lee</h1>
+		<div class="text-info">Fullstack Web Developer</div>
+		<a href="https://blog.s3wn.com" target="_blank">Blog</a>
+		<a href="https://github.com/s3ungw00n" target="_blank">Github</a>
+		<a href="https://twitter.com/s3ungw00n" target="_blank">Twitter</a>
+	</div>
+</header>
+
+<style>
+	.header {
+		min-height: 95vh;
+	}
+
+	.greeting {
+		margin-top: 10rem;
+	}
+</style>
