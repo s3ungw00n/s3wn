f8cdb795d06a5c819940bdd90f53ca3a6ceafe50
LeeSeungWoon <s3wn.code@gmail.com>
2022-06-14 23:55:15 +0900

    Fis formatting

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,26 @@
 <svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
-	/>
+  <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"
+  />
 </svelte:head>
 <header class="header">
-	<div class="greeting px-4">
-		<div>{"Hello, There. I'm"}</div>
-		<h1>SeungWoon Lee</h1>
-		<div class="text-info">Fullstack Web Developer</div>
-		<a href="https://blog.s3wn.com" target="_blank">Blog</a>
-		<a href="https://github.com/s3ungw00n" target="_blank">Github</a>
-		<a href="https://twitter.com/s3ungw00n" target="_blank">Twitter</a>
-	</div>
+  <div class="greeting px-4">
+    <div>{"Hello, There. I'm"}</div>
+    <h1>SeungWoon Lee</h1>
+    <div class="text-info">Fullstack Web Developer</div>
+    <a href="https://blog.s3wn.com" target="_blank">Blog</a>
+    <a href="https://github.com/s3ungw00n" target="_blank">Github</a>
+    <a href="https://twitter.com/s3ungw00n" target="_blank">Twitter</a>
+  </div>
 </header>
 
 <style>
-	.header {
-		min-height: 95vh;
-	}
+  .header {
+    min-height: 95vh;
+  }
 
-	.greeting {
-		margin-top: 10rem;
-	}
+  .greeting {
+    margin-top: 10rem;
+  }
 </style>
